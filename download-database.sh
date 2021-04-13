f3f18b37e783011e31ab542f1d79fc1f827dc1a9
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-04-13 07:33:50 -0300

    Não marca container do mongo para remoção

--- download-database.sh
+++ download-database.sh
@@ -5,7 +5,7 @@ ssh -p 2099 -tt boca@200.128.51.30 "sudo docker exec -ti jude_db_1 mongodump --g
 scp -P 2099 boca@200.128.51.30:/tmp/bocamongo.gz .
 ssh -p 2099 -tt boca@200.128.51.30 "sudo docker exec -ti jude_db_1 rm -f /tmp/bocamongo.gz && sudo rm -f /tmp/bocamongo.gz"
 # Roda servidor mongo local e restaura
-docker run --name jude-mongo -p 27017:27017 -v $PWD:/tmp --rm -d mongo:3.6
+docker run --name jude-mongo -p 27017:27017 -v $PWD:/tmp -d mongo:3.6
 # Aguarda servidor mongo iniciar
 until [ $(docker logs --tail all jude-mongo | grep "waiting for connections on port" | wc -l) -gt 0 ]; do
     printf '.'
