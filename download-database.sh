ee8cbe47bd090661bb4982e2a81e8be93bd767b8
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-04-13 07:30:52 -0300

    Consulta log do container pra saber quando mongo iniciou

--- download-database.sh
+++ download-database.sh
@@ -6,7 +6,9 @@ scp -P 2099 boca@200.128.51.30:/tmp/bocamongo.gz .
 ssh -p 2099 -tt boca@200.128.51.30 "sudo docker exec -ti jude_db_1 rm -f /tmp/bocamongo.gz && sudo rm -f /tmp/bocamongo.gz"
 # Roda servidor mongo local e restaura
 docker run --name jude-mongo -p 27017:27017 -v $PWD:/tmp --rm -d mongo:3.6
-while ! nc -z localhost 27017; do
-  sleep 0.1 # wait for 1/10 of the second before check again
+# Aguarda servidor mongo iniciar
+until [ $(docker logs --tail all jude-mongo | grep "waiting for connections on port" | wc -l) -gt 0 ]; do
+    printf '.'
+    sleep 0.5
 done
 docker exec jude-mongo mongorestore --drop --gzip --archive=/tmp/bocamongo.gz
