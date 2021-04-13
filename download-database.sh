80cc0c2b609b1e6ec8b388c3e8258697e371f964
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-04-13 07:07:53 -0300

    Força uso de pseudo-tty

--- download-database.sh
+++ download-database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Faz dump e copia
-ssh -p 2099 -t boca@200.128.51.30 "sudo docker exec -ti jude_db_1 mongodump --gzip --archive=/tmp/bocamongo.gz && sudo docker cp jude_db_1:/tmp/bocamongo.gz /tmp/"
+ssh -p 2099 -tt boca@200.128.51.30 "sudo docker exec -ti jude_db_1 mongodump --gzip --archive=/tmp/bocamongo.gz && sudo docker cp jude_db_1:/tmp/bocamongo.gz /tmp/"
 scp -P 2099 boca@200.128.51.30:/tmp/bocamongo.gz .
-ssh -p 2099 -t boca@200.128.51.30 "sudo docker exec -ti jude_db_1 rm -f /tmp/bocamongo.gz && sudo rm -f /tmp/bocamongo.gz"
+ssh -p 2099 -tt boca@200.128.51.30 "sudo docker exec -ti jude_db_1 rm -f /tmp/bocamongo.gz && sudo rm -f /tmp/bocamongo.gz"
 # Roda servidor mongo local e restaura
 docker run --name jude-mongo -p 27017:27017 -v $PWD:/tmp -it --rm -d mongo:3.6
 docker exec -ti jude-mongo mongorestore --drop --gzip --archive=/tmp/bocamongo.gz
