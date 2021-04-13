61d01fd097f641fc82106dac3cf5bab6aaaf8b79
Rodrigo Rocha <rodrigorgs@gmail.com>
2021-04-13 07:26:40 -0300

    Aguarda mongo iniciar antes de rodar restore

--- download-database.sh
+++ download-database.sh
@@ -6,4 +6,7 @@ scp -P 2099 boca@200.128.51.30:/tmp/bocamongo.gz .
 ssh -p 2099 -tt boca@200.128.51.30 "sudo docker exec -ti jude_db_1 rm -f /tmp/bocamongo.gz && sudo rm -f /tmp/bocamongo.gz"
 # Roda servidor mongo local e restaura
 docker run --name jude-mongo -p 27017:27017 -v $PWD:/tmp --rm -d mongo:3.6
+while ! nc -z localhost 27017; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
 docker exec jude-mongo mongorestore --drop --gzip --archive=/tmp/bocamongo.gz
